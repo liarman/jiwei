53a10bfc28e68c5c9ad1db22472972bb3896885e
azhanghui <1227978968@qq.com>
2017-12-22 15:18:45 +0800

    检验结果保存

--- tpl/Admin/Public/js/idcard.js
+++ tpl/Admin/Public/js/idcard.js
@@ -17,3 +17,56 @@ function uploadSubmit() {
     });
 }
 
+function ajaxRequest(){
+    var row = $('#IdcardGrid').datagrid('getSelected');
+    if(row==null){
+        $.messager.alert('Warning',"请选择要检验的行", 'info');return false;
+    }
+    if (row){
+        var id=row.id;
+        $.messager.confirm('提示','真的要检验?',function(r){
+            if (r){
+                var durl=lookUrl;
+                $.getJSON(durl,{id:id},function(result){
+                    if (result.status){
+                        $('#IdcardGrid').datagrid('reload');    // reload the user data
+                    } else {
+                        $.messager.alert('错误提示',result.message,'error');
+                    }
+                },'json').error(function(data){
+                    var info=eval('('+data.responseText+')');
+                    $.messager.confirm('错误提示',info.message,function(r){});
+                });
+            }
+        });
+    }
+}
+function  ResponseValue(){
+    var row = $('#IdcardGrid').datagrid('getSelected');
+    if(row==null){
+        $.messager.alert('Warning',"请选择要查看的行", 'info');return false;
+    }
+    if (row){
+        var id=row.id;
+        $('#resopnseValueDlg').dialog('open').dialog('setTitle','检验结果');
+        $('#resopnseValueGrid').datagrid({
+            url:responseUrl+'/idcardid/'+id,
+            columns:[[
+                {field:'cardid',title:'身份证号码',width:150},
+                {field:'name',title:'姓名',width:80},
+                {field:'responsevalue',title:'结果',width:110}
+            ]]
+        });
+    }
+}
+
+function  lookResponseValue(){
+    var row = $('#IdcardGrid').datagrid('getSelected');
+    if(row==null){
+        $.messager.alert('Warning',"请选择要查看的行", 'info');return false;
+    }
+    if (row){
+        $('#lookResponse').dialog('open').dialog('setTitle','查看');
+        $('#lookResponseForm').form('load',row);
+    }
+}
